f3a1b71127d2177ebacef452723c17c8c2359133
Dylan Kario <drk2130@columbia.edu>
2016-03-01 16:16:16 -0500

    Form input populates results

--- app/open-weather/open-weather.module.js
+++ app/open-weather/open-weather.module.js
@@ -1,23 +1,21 @@
 module.exports = [
   '$http',
-  function ($http) {
-    var apiKey = 'd28f5a35ca3977b95c5ab244addbda38';
-    var urlBase = 'http://api.openweathermap.org/data/2.5/weather?q=';
-    var urlSuffix = '&appid=' + apiKey;
+  '$filter',
+  function ($http, $filter) {
 
     return {
-      getWeather: function (city, state) {
-        city.replace(/\s+/g, '');
-        var url = urlBase + city + urlSuffix;
+      getWeather: function (city, country) {
+        var url = getQueryUrl(city, country);
 
         return $http.get(url)
           .then(function (res) {
             return {
               currentWeather: res.data.weather[0].main,
-              temperature: res.data.main.temp,
-              humidity: res.data.main.humidity,
-              sunrise: res.data.sys.sunrise,
-              sunset: res.data.sys.sunrise
+              lastReading: formatDate(new Date()),
+              temperature: formatTemperature(res.data.main.temp),
+              humidity: formatHumidity(res.data.main.humidity),
+              sunrise: formatDate(res.data.sys.sunrise),
+              sunset: formatDate(res.data.sys.sunset)
             };
           }, function (res) {
             console.log('service error');
@@ -26,5 +24,39 @@ module.exports = [
           });
       }
     };
+
+    function getQueryUrl(city, country) {
+      var urlBase = 'http://api.openweathermap.org/data/2.5/weather?q=';
+      var apiKey = 'd28f5a35ca3977b95c5ab244addbda38';
+      var urlSuffix = '&appid=' + apiKey;
+      return urlBase + getLocationString(city, country) + urlSuffix;
+    }
+
+    function formatDate(date) {
+      // If date is UNIX timestamp, convert to ms.
+      if (Number.isInteger(date)) {
+        date *= 1000;
+      }
+      var dateFormat = 'h:mm:ss a';
+      return $filter('date')(date, dateFormat);
+    }
+
+    function formatTemperature(kelvin) {
+      // Convert from Kelvin to Farenheit.
+      return Math.round((9 / 5) * (kelvin - 273) + 32) + ' F';
+    }
+
+    function formatHumidity(humidity) {
+      return humidity + '%';
+    }
+
+    function getLocationString(city, country) {
+      // Ex: 'New York' and 'USA' -> 'newyork,usa
+      return normalize(city) + ',' + normalize(country);
+    }
+
+    function normalize(str) {
+      return str.replace(/\s+/g, '').toLowerCase();
+    }
   }
 ];
--- app/weather/weather.controller.js
+++ app/weather/weather.controller.js
@@ -1,12 +1,24 @@
 module.exports = [
   '$scope',
+  '$filter',
   'openWeather',
-  function ($scope, openWeather) {
-    openWeather.getWeather('newyork')
-      .then(function (data) {
-        $scope.weatherData = data;
-      }, function (data) {
-        console.log('error');
-      });
+  function ($scope, $filter, openWeather) {
+
+    $scope.submit = function () {
+      openWeather.getWeather($scope.city, $scope.country)
+        .then(function (data) {
+          $scope.weatherData = data;
+        }, function (data) {
+          console.log('error');
+        });
+    };
+
+    $scope.init = function () {
+      $scope.city = '';
+      $scope.country = '';
+      $scope.weatherData = null;
+    };
+
+    $scope.init();
   }
 ];
